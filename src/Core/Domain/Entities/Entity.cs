03860da4c24ef1d7f7526ab049a6eb645d8056bc
dgomezs <mikrodj@gmail.com>
2021-04-02 15:34:10 +0200

    refactor: introduce base entity

---
+++ src/Core/Domain/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Domain.Events;
+
+namespace Domain.Entities
+{
+    public abstract class Entity
+    {
+        protected readonly List<DomainEvent> Events;
+
+        public IEnumerable<DomainEvent> DomainEvents => Events;
+
+        protected Entity() =>
+            Events = new List<DomainEvent>();
+    }
+}
--- src/Core/Domain/Entities/TodoList.cs
+++ src/Core/Domain/Entities/TodoList.cs
@@ -1,33 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using Domain.Events;
 using Domain.ValueObjects;
 
 namespace Domain.Entities
 {
-    public class TodoList
+    public class TodoList : Entity
     {
-        private readonly List<DomainEvent> _domainEvents;
-
         public TodoList(TodoListName name, TodoListId id)
         {
             Name = name;
             Id = id;
-            _domainEvents = new List<DomainEvent>();
         }
 
         public TodoList(TodoListName name)
         {
             Id = new TodoListId(Guid.NewGuid());
             Name = name;
-            _domainEvents = new List<DomainEvent>();
-            _domainEvents.Add(new TodoListCreatedEvent(this));
+            Events.Add(new TodoListCreatedEvent(this));
         }
 
         public TodoListName Name { get; }
 
         public TodoListId Id { get; }
-
-        public IEnumerable<DomainEvent> DomainEvents => _domainEvents;
     }
 }
